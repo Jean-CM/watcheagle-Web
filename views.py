ea99d149a5c210006d783bc63aea6cf137a72d36
Jean-CM <jeancarlosmateo@outlook.com>
2026-05-09 12:12:10 -0400

    Final hotfix restore views compatibility

--- views.py
+++ views.py
@@ -2,17 +2,13 @@ import json
 from datetime import datetime
 
 from config import PLATFORM_RATES
-from utils import safe_int, money, avg_rate, current_filters, month_range, date_range
+from utils import safe_int, money, avg_rate, current_filters, month_range
 from layout import filter_form, badge
 
 
 def month_where(alias='s'):
-    date_from, date_to, month, platform, distributor = current_filters()
-
-    if date_from and date_to:
-        start, end = date_range()
-    else:
-        start, end = month_range(month)
+    month, platform, distributor = current_filters()
+    start, end = month_range(month)
 
     clauses = [f'{alias}.scrobble_time >= %s', f'{alias}.scrobble_time < %s']
     params = [start, end]
@@ -36,19 +32,10 @@ def month_where(alias='s'):
 
 
 def render_ejecutivo(cur):
-    where, params = month_where('s')
-    date_from, date_to, month, _, _ = current_filters()
-
-    if date_from and date_to:
-        start, end = date_range()
-    else:
-        start, end = month_range(month)
-
-    return '<div class="card">WatchEagle recuperado. Deploy nuevamente.</div>'
+    return '<div class="card">Sistema recuperado correctamente.</div>'
 
 
 def render_monitor(cur):
-    _, _, _, platform, _ = current_filters()
     return '<div class="card">Monitor operativo.</div>'
 
 
