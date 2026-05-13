8fbc0a7029f322e8626f395886b7ccb9fa6e4fbf
Jean-CM <jeancarlosmateo@outlook.com>
2026-05-13 17:25:07 -0400

    Register strict catalog playlist route

--- app_catalog.py
+++ app_catalog.py
@@ -3,5 +3,7 @@ from app_modular import app
 from helpers import get_conn
 from layout import base_page
 from routes_spotify_catalog import register_spotify_catalog_routes
+from routes_spotify_catalog_strict import register_spotify_catalog_strict_routes
 
 register_spotify_catalog_routes(app, get_conn, base_page)
+register_spotify_catalog_strict_routes(app, get_conn, base_page)
